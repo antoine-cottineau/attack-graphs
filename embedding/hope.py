5bbd14ed3f5997af50cf1aaa59926dbf5c348fa7
antoine-cottineau <antoine.cottineau.dev@gmail.com>
2021-05-05 12:00:37 +0200

    fix a bug when applying HOPE

--- embedding/hope.py
+++ embedding/hope.py
@@ -9,16 +9,24 @@ class Hope(EmbeddingMethod):
     def __init__(self,
                  ag: AttackGraph,
                  dim_embedding: int,
-                 measurement: str = "cn"):
+                 measurement: str = "katz"):
         super().__init__(ag, dim_embedding)
 
         self.measurement = measurement
 
     def embed(self):
-        self.A = self.ag.compute_adjacency_matrix().astype("f")
+        self.A: sps.coo_matrix = self.ag.compute_adjacency_matrix().astype("f")
         self.createS(self.measurement)
 
         U, sigmas, Vt = sps.linalg.svds(self.S, k=int(self.dim_embedding / 2))
+
+        # Organize the singular vectors and singular values in ascending order
+        order = np.flip(np.argsort(sigmas))
+        sigmas = sigmas[order]
+        U = U[:, order]
+        Vt = Vt[order]
+
+        # Compute the embeddings
         sigmas = np.diagflat(np.sqrt(sigmas))
         left_embedding = np.dot(U, sigmas)
         right_embedding = np.dot(Vt.T, sigmas)
@@ -37,27 +45,27 @@ class Hope(EmbeddingMethod):
             self.createSWithAdamicAdar()
 
     def createSWithCommonNeighbours(self):
-        self.S = self.A.dot(self.A)
+        self.S: sps.coo_matrix = self.A.dot(self.A)
 
-    def createSWithKatz(self, beta=0.1):
+    def createSWithKatz(self, beta: float = 0.1):
         Mg = sps.identity(self.ag.number_of_nodes()) - beta * self.A
         Ml = beta * self.A
 
-        self.S = sps.linalg.inv(Mg).dot(Ml)
+        self.S: sps.coo_matrix = sps.linalg.inv(Mg).dot(Ml)
 
-    def createSWithPagerank(self, alpha=0.5):
+    def createSWithPagerank(self, alpha: float = 0.5):
         sum_ = self.A.sum(axis=0)
         sum_ = np.where(sum_ == 0, 1, sum_)
-        P = sps.csc_matrix(self.A / sum_)
+        P = sps.coo_matrix(self.A / sum_)
 
         Mg = sps.identity(self.ag.number_of_nodes()) - alpha * P
 
-        self.S = (1 - alpha) * sps.linalg.inv(Mg)
+        self.S: sps.coo_matrix = (1 - alpha) * sps.linalg.inv(Mg)
 
     def createSWithAdamicAdar(self):
         D = np.zeros((self.ag.number_of_nodes(), self.ag.number_of_nodes()))
         for i in range(self.ag.number_of_nodes()):
             D[i, i] = 1 / (self.A[i].sum() + self.A[:, i].sum())
-        D = sps.csc_matrix(D)
+        D = sps.coo_matrix(D)
 
-        self.S = self.A.dot(D).dot(self.A)
+        self.S: sps.coo_matrix = self.A.dot(D).dot(self.A)
