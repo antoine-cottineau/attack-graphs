6c370b1573d819c5c7836deb9506686534f29296
antoine-cottineau <antoine.cottineau.dev@gmail.com>
2021-05-12 10:42:22 +0200

    refactor the code in dataset

--- report/dataset.py
+++ report/dataset.py
@@ -1,70 +1,96 @@
-import pathlib
 import numpy as np
 import utils
 from attack_graph import AttackGraph
 from attack_graph_generation import Generator
-from time import time
+from pathlib import Path
+from typing import List
 
 
-class DatasetLoader:
-    path_dataset = "methods_input/dataset"
-
-    def __init__(self, max_n_files: int = None):
-        self.files = [
-            file
-            for file in pathlib.Path(DatasetLoader.path_dataset).glob("*.json")
+class Dataset:
+    def __init__(self) -> None:
+        self.set_sizes = [20, 20, 7]
+        # self.set_sizes = [2, 2, 0]
+        self.set_max_n_nodes = [1000, 10000, None]
+        self.n_graphs = sum(self.set_sizes)
+        self.base_path = "methods_input/dataset"
+        self.paths = [
+            Path(self.base_path, label)
+            for label in ["small", "medium", "large"]
         ]
-        self.sort_files()
-        if max_n_files is not None:
-            self.files = self.files[:max_n_files]
-            self.number_of_nodes = self.number_of_nodes[:max_n_files]
-        self.i = 0
 
-    def sort_files(self):
-        # Get the number of nodes of each graph
-        self.number_of_nodes = np.zeros(len(self.files), dtype=int)
-        for i, file in enumerate(self.files):
-            ag = AttackGraph()
-            ag.load(file)
-            self.number_of_nodes[i] = ag.number_of_nodes()
+    def generate_and_save(self):
+        min_complexity = 20
+        max_complexity = 40
 
-        # Sort the attack graphs based on their number of nodes
-        order = np.argsort(self.number_of_nodes)
-        self.number_of_nodes = self.number_of_nodes[order]
-        new_files = []
-        for i in order:
-            new_files.append(self.files[i])
-        self.files = new_files
+        sets: List[List[AttackGraph]] = []
+        for _ in range(len(self.set_sizes)):
+            sets.append([])
 
-    def reinitialize(self):
-        self.i = 0
+        # Generate the graphs
+        are_sets_filled = False
+        complexity = min_complexity
+        while not are_sets_filled:
+            # Generate a graph with the current complexity
+            print("Generating graph with complexity {}".format(complexity))
+            graph = Generator(n_propositions=complexity,
+                              n_exploits=complexity).generate()
 
-    def get(self, i) -> AttackGraph:
-        return self.files[i]
+            # Find the appropriate set for this graph
+            n_nodes = graph.number_of_nodes()
+            graph_set = None
+            i_set = 0
+            while i_set < len(self.set_sizes) and graph_set is None:
+                set_max_n_nodes = self.set_max_n_nodes[i_set]
+                if set_max_n_nodes is None or n_nodes < set_max_n_nodes:
+                    graph_set = i_set
+                i_set += 1
 
-    def __iter__(self):
-        return self
+            print("With {} nodes, this graph belongs to set {}".format(
+                n_nodes, graph_set))
 
-    def __next__(self) -> AttackGraph:
-        if self.i < len(self.files):
-            ag = AttackGraph()
-            ag.load(self.files[self.i])
-            self.i += 1
-            return ag
-        else:
-            raise StopIteration
+            # Compute the number of existing graphs for each set
+            current_set_sizes = [len(set) for set in sets]
 
+            # Check whether we can add the graph or not
+            if current_set_sizes[graph_set] < self.set_sizes[graph_set]:
+                print("The graph has been added to the set")
+                sets[graph_set].append(graph)
+                current_set_sizes[graph_set] += 1
+            else:
+                print("The graph hasn't been added to the set")
 
-def create_dataset(start: int = 20, end: int = 50, quantity: int = 100):
-    complexities = np.linspace(start, end, quantity, dtype=int)
-    base_path = "methods_input/dataset"
-    utils.create_folders(base_path)
-    for i, complexity in enumerate(complexities):
-        print("Generating graph with complexity {}".format(complexity))
-        generator = Generator(n_propositions=complexity, n_exploits=complexity)
-        start = time()
-        ag = generator.generate()
-        ag.save("{}/{}.json".format(base_path, i))
-        print("Generated graph {} with complexity {} and {} nodes in {:.0f}"
-              " seconds".format(i, complexity, ag.number_of_nodes(),
-                                time() - start))
+            # Check whether all sets are filled or not
+            are_sets_filled = current_set_sizes == self.set_sizes
+
+            print("Current set sizes: {}\n".format(" ".join(
+                [str(set_size) for set_size in current_set_sizes])))
+
+            # Change the complexity for the next iteration
+            if complexity == max_complexity:
+                complexity = min_complexity
+            else:
+                complexity += 1
+
+        # Save the graphs
+        i_graph = 0
+        for i_set, set in enumerate(sets):
+            # Create the set folder
+            path = self.paths[i_set]
+            utils.create_folders(path)
+
+            # Save the graphs in the order of their number of nodes
+            graphs_n_nodes = [graph.number_of_nodes() for graph in set]
+            order = np.argsort(graphs_n_nodes)
+            for position in order:
+                graph = set[position]
+                graph.save("{}/{}.json".format(path, i_graph))
+                i_graph += 1
+
+    def load(self, i_graph: int) -> AttackGraph:
+        path = [
+            file
+            for file in Path(self.base_path).glob("*/{}.json".format(i_graph))
+        ][0]
+        graph = AttackGraph()
+        graph.load(path)
+        return graph
